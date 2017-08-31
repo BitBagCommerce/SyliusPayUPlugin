d52c9c068eed72373c4ff32127942820e859df73
Mikołaj Król <mikolaj.krol@bitbag.pl>
2017-08-31 19:50:02 +0200

    [General][Behat] Cleanup, Add Behat requirement to composer

--- tests/Application/app/AppKernel.php
+++ tests/Application/app/AppKernel.php
@@ -29,7 +29,7 @@ final class AppKernel extends Kernel
             new \FOS\OAuthServerBundle\FOSOAuthServerBundle(), // Required by SyliusApiBundle
             new \Sylius\Bundle\AdminApiBundle\SyliusAdminApiBundle(),
 
-            new BitBag\PayUPlugin\BitBagPayUPlugin(),
+            new \BitBag\PayUPlugin\BitBagPayUPlugin(),
         ]);
     }
 
