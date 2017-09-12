c690a471a10e974ecc31008ac121817c8a3c821b
Mikołaj Król <mikolaj.krol@bitbag.pl>
2017-09-12 23:22:27 +0200

    [General] Upgrade Sylius to 1.0+ and PHP to 7.1+

--- tests/Application/app/AppKernel.php
+++ tests/Application/app/AppKernel.php
@@ -1,26 +1,17 @@
 <?php
 
-/**
- * This file was created by the developers from BitBag.
- * Feel free to contact us once you face any issues or want to start
- * another great project.
- * You can find more information about us on https://bitbag.shop and write us
- * an email on kontakt@bitbag.pl.
- */
+declare(strict_types=1);
 
 use Sylius\Bundle\CoreBundle\Application\Kernel;
 use Symfony\Component\Config\Loader\LoaderInterface;
 use PSS\SymfonyMockerContainer\DependencyInjection\MockerContainer;
 
-/**
- * @author Patryk Drapik <patryk.drapik@bitbag.pl>
- */
-final class AppKernel extends Kernel
+class AppKernel extends Kernel
 {
     /**
      * {@inheritdoc}
      */
-    public function registerBundles()
+    public function registerBundles(): array
     {
         return array_merge(parent::registerBundles(), [
             new \Sylius\Bundle\AdminBundle\SyliusAdminBundle(),
@@ -36,7 +27,7 @@ final class AppKernel extends Kernel
     /**
      * {@inheritdoc}
      */
-    public function registerContainerConfiguration(LoaderInterface $loader)
+    public function registerContainerConfiguration(LoaderInterface $loader): void
     {
         $loader->load($this->getRootDir() . '/config/config.yml');
     }
@@ -44,11 +35,12 @@ final class AppKernel extends Kernel
     /**
      * {@inheritdoc}
      */
-    protected function getContainerBaseClass()
+    protected function getContainerBaseClass(): string
     {
         if ('test' === $this->environment) {
             return MockerContainer::class;
         }
+
         return parent::getContainerBaseClass();
     }
 }
