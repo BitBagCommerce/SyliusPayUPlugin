b5d772c2438ec4d28ae0e3ac7c402f6c5ad92bfc
Michał Kalkowski <michal.kalkowski@bitbag.pl>
2019-05-09 11:36:24 +0200

    Upgrade to Sylius 1.4
    Change Yaml to Xml file loader in plugin container extension
    Add symlink to node_modules catalog

--- src/DependencyInjection/BitBagSyliusPayUExtension.php
+++ src/DependencyInjection/BitBagSyliusPayUExtension.php
@@ -25,8 +25,7 @@ final class BitBagSyliusPayUExtension extends Extension
      */
     public function load(array $configs, ContainerBuilder $container)
     {
-        $loader = new Loader\YamlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
-
-        $loader->load('services.yml');
+        $loader = new Loader\XmlFileLoader($container, new FileLocator(__DIR__.'/../Resources/config'));
+        $loader->load('services.xml');
     }
 }
