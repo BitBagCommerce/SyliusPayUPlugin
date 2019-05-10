039321d81c20ec95d6037ab3d22ea73297b662e4
Michał Kalkowski <michal.kalkowski@bitbag.pl>
2019-05-10 14:33:04 +0200

    Upgrade to Sylius 1.4
    Add strict type and type hinting

--- src/PayUGatewayFactory.php
+++ src/PayUGatewayFactory.php
@@ -8,54 +8,49 @@
  * an email on kontakt@bitbag.pl.
  */
 
+declare(strict_types=1);
+
 namespace BitBag\SyliusPayUPlugin;
 
 use BitBag\SyliusPayUPlugin\Action\CaptureAction;
 use BitBag\SyliusPayUPlugin\Action\ConvertPaymentAction;
-use BitBag\SyliusPayUPlugin\Action\NotifyAction;
 use BitBag\SyliusPayUPlugin\Action\StatusAction;
-use BitBag\SyliusPayUPlugin\Bridge\OpenPayUBridge;
+use BitBag\SyliusPayUPlugin\Bridge\OpenPayUBridgeInterface;
 use Payum\Core\Bridge\Spl\ArrayObject;
 use Payum\Core\GatewayFactory;
 
-/**
- * @author Mikołaj Król <mikolaj.krol@bitbag.pl>
- */
 final class PayUGatewayFactory extends GatewayFactory
 {
-    /**
-     * {@inheritDoc}
-     */
-    protected function populateConfig(ArrayObject $config)
+    protected function populateConfig(ArrayObject $config): void
     {
-        $config->defaults([
-            'payum.factory_name' => 'payu',
-            'payum.factory_title' => 'PayU',
+        $config->defaults(
+            [
+                'payum.factory_name' => 'payu',
+                'payum.factory_title' => 'PayU',
 
-            'payum.action.capture' => new CaptureAction(),
-            'payum.action.convert_payment' => new ConvertPaymentAction(),
-            'payum.action.status' => new StatusAction(),
-        ]);
+                'payum.action.capture' => new CaptureAction(),
+                'payum.action.convert_payment' => new ConvertPaymentAction(),
+                'payum.action.status' => new StatusAction(),
+            ]
+        );
 
-        if (false == $config['payum.api']) {
+        if (false === (bool) $config['payum.api']) {
             $config['payum.default_options'] = [
-                'environment' => 'secure',
+                'environment' => OpenPayUBridgeInterface::SANDBOX_ENVIRONMENT,
                 'pos_id' => '',
-                'signature_key' => ''
+                'signature_key' => '',
             ];
             $config->defaults($config['payum.default_options']);
             $config['payum.required_options'] = ['environment', 'pos_id', 'signature_key'];
 
-            $config['payum.api'] = function (ArrayObject $config) {
+            $config['payum.api'] = static function (ArrayObject $config): array {
                 $config->validateNotEmpty($config['payum.required_options']);
 
-                $payuConfig = [
+                return [
                     'environment' => $config['environment'],
                     'pos_id' => $config['pos_id'],
                     'signature_key' => $config['signature_key'],
                 ];
-
-                return $payuConfig;
             };
         }
     }
