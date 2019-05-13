c7c949a4b23235267d9e8d53a676223301114e80
Michał Kalkowski <michal.kalkowski@bitbag.pl>
2019-05-13 14:17:49 +0200

    Upgrade to Sylius 1.4
    Add oAuth authentication fields to form

--- src/Bridge/OpenPayUBridge.php
+++ src/Bridge/OpenPayUBridge.php
@@ -18,16 +18,30 @@ use OpenPayU_Result;
 
 final class OpenPayUBridge implements OpenPayUBridgeInterface
 {
-    public function setAuthorizationDataApi(string $environment, string $signatureKey, string $posId): void
-    {
+    public function setAuthorizationData(
+        string $environment,
+        string $signatureKey,
+        string $posId,
+        string $clientId,
+        string $clientSecret
+    ): void {
         OpenPayU_Configuration::setEnvironment($environment);
-        OpenPayU_Configuration::setSignatureKey($signatureKey);
+
+        //set POS ID and Second MD5 Key (from merchant admin panel)
         OpenPayU_Configuration::setMerchantPosId($posId);
+        OpenPayU_Configuration::setSignatureKey($signatureKey);
+
+        //set Oauth Client Id and Oauth Client Secret (from merchant admin panel)
+        OpenPayU_Configuration::setOauthClientId($clientId);
+        OpenPayU_Configuration::setOauthClientSecret($clientSecret);
     }
 
-    public function create(array $order)
+    public function create(array $order): ?OpenPayU_Result
     {
-        return OpenPayU_Order::create($order);
+        /** @var null|OpenPayU_Result $result */
+        $result = OpenPayU_Order::create($order);
+
+        return $result;
     }
 
     public function retrieve(string $orderId): OpenPayU_Result
--- src/Bridge/OpenPayUBridgeInterface.php
+++ src/Bridge/OpenPayUBridgeInterface.php
@@ -16,21 +16,29 @@ use OpenPayU_Result;
 
 interface OpenPayUBridgeInterface
 {
-    public const SANDBOX_ENVIRONMENT = 'sandbox';
-    public const NEW_API_STATUS = 'NEW';
-    public const PENDING_API_STATUS = 'PENDING';
-    public const COMPLETED_API_STATUS = 'COMPLETED';
-    public const SUCCESS_API_STATUS = 'SUCCESS';
-    public const CANCELED_API_STATUS = 'CANCELED';
-    public const COMPLETED_PAYMENT_STATUS = 'COMPLETED';
-    public const PENDING_PAYMENT_STATUS = 'PENDING';
-    public const CANCELED_PAYMENT_STATUS = 'CANCELED';
+    public const SANDBOX_ENVIRONMENT                     = 'sandbox';
+    public const SECURE_ENVIRONMENT                      = 'secure';
+
+    public const NEW_API_STATUS                          = 'NEW';
+    public const PENDING_API_STATUS                      = 'PENDING';
+    public const COMPLETED_API_STATUS                    = 'COMPLETED';
+    public const SUCCESS_API_STATUS                      = 'SUCCESS';
+    public const CANCELED_API_STATUS                     = 'CANCELED';
+    public const COMPLETED_PAYMENT_STATUS                = 'COMPLETED';
+    public const PENDING_PAYMENT_STATUS                  = 'PENDING';
+    public const CANCELED_PAYMENT_STATUS                 = 'CANCELED';
     public const WAITING_FOR_CONFIRMATION_PAYMENT_STATUS = 'WAITING_FOR_CONFIRMATION';
-    public const REJECTED_STATUS = 'REJECTED';
+    public const REJECTED_STATUS                         = 'REJECTED';
 
-    public function setAuthorizationDataApi(string $environment, string $signatureKey, string $posId): void;
+    public function setAuthorizationData(
+        string $environment,
+        string $signatureKey,
+        string $posId,
+        string $clientId,
+        string $clientSecret
+    ): void;
 
-    public function create(array $order);
+    public function create(array $order): ?OpenPayU_Result;
 
     public function retrieve(string $orderId): OpenPayU_Result;
 
