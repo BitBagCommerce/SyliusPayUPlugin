00649a177a61588098f4c7c7cd60734227542448
Marcin Kukliński <senghe@gmail.com>
2026-05-13 08:52:16 +0200

    Add Sylius 1.14 support; drop Sylius 1.12 and 1.13 support.

--- tests/Application/config/sylius/1.14/bundles.php
+++ tests/Application/config/sylius/1.14/bundles.php
@@ -9,8 +9,8 @@
 declare(strict_types=1);
 
 return [
-    SyliusLabs\Polyfill\Symfony\Security\Bundle\SyliusLabsPolyfillSymfonySecurityBundle::class => ['all' => true],
     Sylius\Calendar\SyliusCalendarBundle::class => ['all' => true],
-    League\FlysystemBundle\FlysystemBundle::class =>['all' => true],
+    League\FlysystemBundle\FlysystemBundle::class => ['all' => true],
     Sylius\Abstraction\StateMachine\SyliusStateMachineAbstractionBundle::class => ['all' => true],
+    ApiPlatform\Symfony\Bundle\ApiPlatformBundle::class => ['all' => true],
 ];
